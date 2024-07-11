d91437b764adbe63cd9b9f9297107f6c60beedd2
achdikiprasetyo <dikiprasetyo1234@gmail.com>
2024-07-11 09:55:20 +0700

    qr

---
+++ resources/views/qrcode.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Generate QR Code</title>
+</head>
+<body>
+    <div>
+        {!! $qrCode !!}
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QrCodeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/generate-qrcode', 'QrCodeController@generate');
+Route::get('/generate-qrcode', [QrCodeController::class, 'generate']);
