862faf143f4c646c92e7e7e3c08b597323577ccd
bagasseptyono <muhammadbagasmbs@gmail.com>
2024-07-11 13:21:52 +0700

    add item on room

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -9,20 +9,20 @@ use Illuminate\Http\Request;
 class ItemController extends Controller
 {
     public function index(Room $room)
-{
-    // Menggunakan eager loading untuk memuat relasi items
-    $room = Room::with('items')->find($room->id);
+    {
+        // Menggunakan eager loading untuk memuat relasi items
+        $room = Room::with('items')->find($room->id);
 
-    // Mengecek apakah room ditemukan
-    if (!$room) {
-        abort(404); // Jika room tidak ditemukan, tampilkan 404 error
+        // Mengecek apakah room ditemukan
+        if (!$room) {
+            abort(404); // Jika room tidak ditemukan, tampilkan 404 error
+        }
+
+        // Mengambil semua items yang terkait dengan room
+        $items = $room->items;
+
+        return view('items.index', compact('items', 'room'));
     }
-
-    // Mengambil semua items yang terkait dengan room
-    $items = $room->items;
-
-    return view('items.index', compact('items', 'room'));
-}
 
 
     public function show(Item $item)
@@ -45,7 +45,7 @@ class ItemController extends Controller
     {
         return view('items.create', compact('room'));
     }
-    
+
 
     public function store(Request $request)
     {
